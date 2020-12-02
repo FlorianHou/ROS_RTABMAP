93625f8a6a283b3bf4238049efe7399efc8e9a6e
Florian Hou <florianhou@hotmail.com>
2020-12-02 09:04:45 +0100

    2020-12-02 Wählen Speichen oder nicht nachdem Mapping

--- ShellSkript/rtabmap.sh
+++ ShellSkript/rtabmap.sh
@@ -2,7 +2,20 @@
 name=$(date +"%Y%m%d_%H%M%S").db
 path=~/ROS/map-dbs/$name
 
+echo $1
 roslaunch rtabmap_ros d455_rtabmap.launch
 
-cp ~/.ros/rtabmap.db $path
-ls -l ~/ROS/map-dbs/
+# DB_MAP speichen oder nicht::
+echo "Willst du dises Map "$name" speichen?: Ja(1) / Nein(2)"
+select wahl in "Ja" "Nein"; do
+    case $wahl in
+        Ja          ) 
+            cp ~/.ros/rtabmap.db $path;
+            echo "gespeichert";
+            ls -l ~/ROS/map-dbs/;
+            break;;
+        Nein        )  
+            echo "gelöst"; break;
+            ls -l ~/ROS/map-dbs/;;
+    esac
+done
