9dbf7af63e4c383c8b82af2b8215f27d6b8f0309
Florian Hou <florianhou@hotmail.com>
2021-01-21 18:57:15 +0100

    Record mit lz4

--- ShellSkript/record.sh
+++ ShellSkript/record.sh
@@ -4,7 +4,7 @@ name=$(date +"%Y%m%d_%H%M%S").bag
 path=~/ROS/bags/$name
 echo $path
 
-rosbag record -O $path \
+rosbag record --lz4 -O $path \
         /camera/aligned_depth_to_color/camera_info \
         /camera/aligned_depth_to_color/image_raw \
         /camera/color/camera_info \
